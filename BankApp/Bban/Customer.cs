e8f584897d97b1a51e70956ed55847733846d086
Joonas Leminen <kaktus994hotmail.com>
2018-03-25 10:00:36 +0300

    Initial commit

---
+++ BankApp/Bban/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BankApp
+{
+    class Customer
+    {
+        
+        private string _firstName;
+        private string _lastName;
+        private string _accountNumber;
+
+        
+        public Customer(string firstName, string lastName, string accountNumber)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _accountNumber = accountNumber;
+        }
+
+        public string AccountNumber { get => _accountNumber; set => _accountNumber = value; }
+
+        public override string ToString()
+        {
+            return $"{_firstName}{_lastName}\t{_accountNumber}";
+        }
+    }
+}
